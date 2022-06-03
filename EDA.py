e1d746d924e49037ff573ad751f0cbfd2f7efa54
nickhitt <73009988+nickhitt@users.noreply.github.com>
2022-06-03 23:48:32 +1200

    EDA

---
+++ EDA.py
@@ -0,0 +1,39 @@
+exec(open("load_data.py").read())
+
+### Distribution of target vars
+
+fig, ax = plt.subplots(figsize=(10,5))
+sns.countplot(x=train_labels.target)
+plt.show()
+
+train_labels['target'].value_counts() #counts
+
+### Merging training labels with training df
+
+train_df = train_df.merge(train_labels, on='customer_ID', how='left')
+
+# Function to calculate missing values by column
+def missing_values_table(df):
+    # Total missing values
+    mis_val = df.isnull().sum()
+    # Percentage of missing values
+    mis_val_percent = 100 * mis_val / len(df)
+    # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+    # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(
+        columns={0: 'Missing Values', 1: '% of Total Values'})
+    # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+    # Print some summary information
+    print("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+          "There are " + str(mis_val_table_ren_columns.shape[0]) +
+          " columns that have missing values.")
+    # Return the dataframe with missing information
+    return mis_val_table_ren_columns
+
+train_missing_vals = missing_values_table(train_df)
+
+train_missing_vals.head(10)
--- load_data.py
+++ load_data.py
@@ -3,11 +3,13 @@ import numpy as np
 import sklearn as skl
 import scipy as sc
 import statsmodels as sm
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
 ### Data Load
 
 train_df = pd.read_feather('/Users/nicholashitt/Dropbox/My Mac (Nicholas’s MacBook Pro)/Downloads/archive (2)/train.feather')
 test_df = pd.read_feather('/Users/nicholashitt/Dropbox/My Mac (Nicholas’s MacBook Pro)/Downloads/archive (2)/test.feather')
 train_labels = pd.read_csv('/Users/nicholashitt/Dropbox/My Mac (Nicholas’s MacBook Pro)/Downloads/train_labels.csv')
-train_df.shape, test_df.shape
+
