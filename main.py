02cb31731890621cf5fd19923e67d1042f24b372
nickhitt <73009988+nickhitt@users.noreply.github.com>
2022-06-03 22:10:28 +1200

    data load

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import numpy as np
 import sklearn as skl
 import scipy as sc
 import statsmodels as sm
+
+
+### Data Load
+
+train_df = pd.read_feather('/Users/nicholashitt/Dropbox/My Mac (Nicholas’s MacBook Pro)/Downloads/archive (2)/train.feather')
+
