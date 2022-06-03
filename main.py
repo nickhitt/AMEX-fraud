d7ab3883cf0fffb12326f02d6b812047969d2ca7
nickhitt <73009988+nickhitt@users.noreply.github.com>
2022-06-03 23:26:07 +1200

    data load

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ import statsmodels as sm
 ### Data Load
 
 train_df = pd.read_feather('/Users/nicholashitt/Dropbox/My Mac (Nicholas’s MacBook Pro)/Downloads/archive (2)/train.feather')
-
+test_df = pd.read_feather('/Users/nicholashitt/Dropbox/My Mac (Nicholas’s MacBook Pro)/Downloads/archive (2)/test.feather')
+train_labels = pd.read_csv('/Users/nicholashitt/Dropbox/My Mac (Nicholas’s MacBook Pro)/Downloads/train_labels.csv')
+train_df.shape, test_df.shape
