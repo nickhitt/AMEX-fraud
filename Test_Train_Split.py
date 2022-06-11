3178d46c4b6fbea818ffd3f5088614284e951fba
nickhitt <73009988+nickhitt@users.noreply.github.com>
2022-06-11 16:00:19 +1200

    EDA distribution of target var - fixed missing values - not issue for HGB - but to resolve with KNN imputation for future NN modeling Added encoding - aligned dfs correlations + heatmap
    Modelling
    Test Train Splits - resolved class imbalance + scaled data
    Added HGBC - added f1
    KNN - not useful because of missing data, changing filename to Model_NN to begin NN modelling work
    
    Separated Test Train Split into separate script
    Renamed Model -> HGB
    Began HGB model tuning

--- Test_Train_Split.py
+++ Test_Train_Split.py
@@ -1,9 +1,8 @@
 import pandas as pd
 from sklearn import model_selection
-from random import sample
+from sklearn.preprocessing import StandardScaler
 
 ### Splitting into train/test sets
-
 
 count_pos = train_df[train_df['target'] == 1]['target'].count().astype(int)
 count_pos = count_pos.astype(int)
@@ -24,5 +23,7 @@ y_train = train_data['target']
 X_test = valid_data.drop(['customer_ID', 'S_2', 'target'], axis=1)
 y_test = valid_data['target']
 
-#X_real_test = test_df.drop(['customer_ID', 'S_2'], axis=1)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
